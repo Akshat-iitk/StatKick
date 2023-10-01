de9375ade62372f2aebd9a353ed3fed2d0d8f2a6
Akshatiitk <akshatt21@iitk.ac.in>
2023-10-01 17:50:51 +0530

    button initialised

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { React } from 'react';
 import Navbar from './Components/Navbar';
 import Homepage from './Components/Homepage';
+import Button from './Components/button';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
       <Homepage/>
+     <Button/>
      <h1>STAT KICK</h1>
     </div>
   );
---
+++ src/Components/button.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Button() {
+  return (
+    <div>
+      <button> this is a button</button>
+    </div>
+  )
+}
